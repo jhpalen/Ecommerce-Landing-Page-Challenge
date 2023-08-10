43fa730c69403fbc00b8aab8bad9481922905992
John Hazon Gacutan Palen <jhpalen@Kween-Haze.local>
2023-08-10 10:04:21 +0800

    Initial Setup: Set up project structure, HTML, CSS, and JavaScript files.

---
+++ Assets/scripts.js
@@ -0,0 +1,14 @@
+// scripts.js
+$(document).ready(function() {
+    $('#navigation').load('Sections/navigation.html');
+    $('#benefits').load('Sections/benefits.html');
+    $('#hero').load('Sections/hero.html');
+    $('#sitewideDeals').load('Sections/sitewideDeals.html');
+    $('#vipExclusive').load('Sections/vipExclusive.html');
+    $('#trending').load('Sections/trending.html');
+    $('#newIn').load('Sections/newIn.html');
+    $('#recentlyBought').load('Sections/recentlyBought.html');
+    $('#insta').load('Sections/insta.html');
+    $('#newsletter').load('Sections/newsletter.html');
+    $('#footer').load('Sections/footer.html');
+});
