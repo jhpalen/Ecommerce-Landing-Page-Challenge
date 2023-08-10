5091df21e5ec5846b887a77e3ea0ec82bcae3828
John Hazon Gacutan Palen <jhpalen@Kween-Haze.local>
2023-08-10 12:25:10 +0800

    Navigation Section: Added navigation section with links to different parts of the page.

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,6 +1,17 @@
 // scripts.js
 $(document).ready(function() {
-    $('#navigation').load('Sections/navigation.html');
+    $('#navigation').load('Sections/navigation.html', function() {
+        // This callback runs after the content is loaded
+        // Add event listeners or other code that relies on the loaded content
+        const hamburger = document.querySelector('.hamburger');
+        const mainMenu = document.querySelector('.main-menu');
+
+        if (hamburger && mainMenu) {
+            hamburger.addEventListener('click', () => {
+                mainMenu.classList.toggle('active');
+            });
+        }
+    });
     $('#benefits').load('Sections/benefits.html');
     $('#hero').load('Sections/hero.html');
     $('#sitewideDeals').load('Sections/sitewideDeals.html');
